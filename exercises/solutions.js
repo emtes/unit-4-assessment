2ed0678e0549a18925de2558d4f467c0826514ca
EC2 Default User <ec2-user@ip-172-31-47-173.us-east-2.compute.internal>
2019-12-20 17:38:11 +0000

    fill exercise solutions v1

--- exercises/solutions.js
+++ exercises/solutions.js
@@ -1,13 +1,89 @@
+//1
+function makeAccount(startingBal) {
+  let balance = startingBal;
+  return {
+    checkBalance() {
+      return balance;
+    },
+    add(amount) {
+      balance += amount;
+      return `${amount} added`;
+    },
+    subtract(amount) {
+      balance -= amount;
+      return `${amount} subtracted`;
+    },
+  };
+}
 
+//2
+function SavingsAccount(name) {
+  this.name = name;
+  this.balance = 0;
+}
+
+SavingsAccount.prototype = {
+  constructor: SavingsAccount,
+  showBalance() {
+    return `$${this.balance}`;
+  },
+  depositFunds(amount) {
+    const invalVal = "Please include a deposit amount that is greater than 0";
+    if (amount < 0) return invalVal;
+    this.balance += amount;
+    return `$${amount} Deposited`;
+  },
+  withdrawFunds(amount) {
+    if (amount > this.balance) return "Insufficient Funds";
+    this.balance -= amount;
+    return `$${amount} withdrawn`;
+  },
+};
+
+//3
+function Phone(number) {
+  this.phoneNumber = number;
+  this.contacts = [];
+}
+
+Phone.prototype = {
+  constructor: Phone,
+  addContact(contact) {
+    if (contact.name.length <= 0 || !contact.phoneNumber.test(/^\d{10}/g)) {
+      return "Invalid";
+    }
+    this.contacts.push(contact);
+    return `${contact.name} added.`;
+  },
+  removeContact(name) {
+    if (this.contacts.some(contact => contact.name === name)) {
+      this.contacts.splice(this.contacts.indexOf(this.contacts.filter(c => c.name === name)[0]), 1);
+      return `${name} removed.`;
+    } else {
+      return "Contact not found.";
+    }
+  },
+  call(number) {
+    if (!number.test(/^\d{10}/g) || this.contacts.some(contact => contact.number === number)) {
+      return "Invalid";
+    }
+
+    if (this.contacts.some(contact => contact.number === number)) {
+      const contactIndex = this.contacts.indexOf(this.contacts.filter(c => c.number === number)[0]);
+      const contactName = this.contacts[contactIndex].name;
+      return `Calling ${contactName}...`;
+    }
+  },
+};
 
 module.exports = {
-  // makeAccount,
-  // SavingsAccount
-  // Phone
-  // AppleiPhone
-  // Person
-  // Student
-  // GraduateStudent
-  // Professor
-  // Doctor
+  makeAccount,
+  SavingsAccount,
+  Phone,
+  // AppleiPhone,
+  // Person,
+  // Student,
+  // GraduateStudent,
+  // Professor,
+  // Doctor,
 };
--- exercises/solutions.test.js
+++ exercises/solutions.test.js
@@ -14,28 +14,28 @@ test('Question 1', () => {
 test('Question 2', () => {
   const myAccount = new solutions.SavingsAccount('Reuben');
   expect(myAccount.name).toBe('Reuben');
-  expect(myAccount.balance).toBe('$0');
+  expect(myAccount.showBalance()).toBe('$0');
   expect(myAccount.depositFunds(100)).toBe('$100 Deposited');
-  expect(myAccount.balance).toBe('$100');
+  expect(myAccount.showBalance()).toBe('$100');
   expect(myAccount.depositFunds(-100)).toBe('Please include a deposit amount that is greater than 0');
-  expect(myAccount.balance).toBe('$100');
+  expect(myAccount.showBalance()).toBe('$100');
   expect(myAccount.withdrawFunds(75)).toBe('$75 withdrawn');
-  expect(myAccount.balance).toBe('$25');
+  expect(myAccount.showBalance()).toBe('$25');
   expect(myAccount.withdrawFunds(26)).toBe('Insufficient Funds');
-  expect(myAccount.balance).toBe('$25');
+  expect(myAccount.showBalance()).toBe('$25');
 });
 
 // Question 3
 test('Question 3', () => {
   const myPhone = new solutions.Phone('3448731233');
-  expect(myPhone.addContact({name: "Reuben", phoneNumber: '3462217541'})).toBe('Reuben added.');
-  expect(myPhone.contacts).toEqual([{name: "Reuben", phoneNumber: '3462217541'}]);
-  expect(myPhone.addContact({phoneNumber: '3461124321'})).toBe('Invalid');
-  expect(myPhone.addContact({name: "Reuben", phoneNumber: '346'})).toBe('Invalid');
-  expect(myPhone.addContact({name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine'})).toBe('Peter added.');
-  expect(myPhone.contacts).toEqual([{name: "Reuben", phoneNumber: '3462217541'}, {name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine'}]);
+  expect(myPhone.addContact({ name: "Reuben", phoneNumber: '3462217541' })).toBe('Reuben added.');
+  expect(myPhone.contacts).toEqual([{ name: "Reuben", phoneNumber: '3462217541' }]);
+  expect(myPhone.addContact({ phoneNumber: '3461124321' })).toBe('Invalid');
+  expect(myPhone.addContact({ name: "Reuben", phoneNumber: '346' })).toBe('Invalid');
+  expect(myPhone.addContact({ name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine' })).toBe('Peter added.');
+  expect(myPhone.contacts).toEqual([{ name: "Reuben", phoneNumber: '3462217541' }, { name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine' }]);
   expect(myPhone.removeContact('Reuben')).toBe('Reuben removed.');
-  expect(myPhone.contacts).toEqual([{name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine'}]);
+  expect(myPhone.contacts).toEqual([{ name: "Peter", phoneNumber: '3499217541', extraData: 'is perfectly fine' }]);
   expect(myPhone.removeContact('Paul')).toBe('Contact not found.');
   expect(myPhone.call('Peter')).toBe('Contact not found.');
   expect(myPhone.call('3499217541')).toBe('Calling Peter...');
@@ -49,8 +49,8 @@ test('Question 4', () => {
   const reubenPhone = new solutions.AppleiPhone('2234432211', 'iPhone 14');
   const mayasPhone = new solutions.AppleiPhone('3349987745', 'iPhone 6');
   const devontesPhone = new solutions.Phone('9932103000');
-  expect(reubensPhone instanceof solutions.Phone).toBe(true);  
-  expect(reubensPhone instanceof solutions.AppleiPhone).toBe(true);  
+  expect(reubensPhone instanceof solutions.Phone).toBe(true);
+  expect(reubensPhone instanceof solutions.AppleiPhone).toBe(true);
   expect(reubensPhone.sendiMessage(mayasPhone, 'Hey friend!')).toBe('Message sent.');
   expect(mayasPhone.sendiMessage(reubensPhone, 'Hey!')).toBe('Message sent.');
   expect(reubensPhone.sendiMessage(devontesPhone, 'DYFR!')).toBe('Message could not be sent.');
